6674cef971014b9a86525dbba0935925364469eb
Eylul <82230150+Eylulbasak@users.noreply.github.com>
2024-03-11 20:01:22 +0300

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,108 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 500)
-df = sns.load_dataset('titanic')
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.keras.preprocessing.image import load_img
 
-cat_cols = [col for col in df.columns if str(df[col].dtypes) in ['category', 'object', 'bool']]
-num_but_col = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ['int', 'float']]
+base_dir = "./data/"
+happy_dir = os.path.join(base_dir, "happy/")
+sad_dir = os.path.join(base_dir, "sad/")
 
-def cat_summary(dataframe, col_name):
-    print(pd.DataFrame({col_name:dataframe[col_name].value_counts(),
-                        'Ratio': 100 * dataframe[col_name].value_counts()/ len(dataframe[col_name])}))
-cat_summary(df, 'sex')
+print("Sample happy image:")
+plt.imshow(load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}"))
+plt.show()
 
-for col in cat_cols:
-    cat_summary(df, col)
-def cat_summary(dataframe, col_name, plot=False):
-    print(pd.DataFrame({col_name:dataframe[col_name].value_counts(),
-                        'Ratio': 100 * dataframe[col_name].value_counts()/ len(dataframe[col_name])}))
+print("\nSample sad image:")
+plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
+plt.show()
+from tensorflow.keras.preprocessing.image import img_to_array
 
-    if plot:
-      sns.countplot(x=dataframe[col_name], data=dataframe)
-      plt.show(block=True)
-cat_summary(df, 'sex', plot=True)
-for col in cat_cols:
-    if df[col].dtypes == 'bool':
-        df[col] = df[col].astype(int)
-        cat_summary(df, col, plot=True)
-    else:
-        cat_summary(df, col, plot=True)
+# Load the first example of a happy face
+sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+
+print(f"Each image has shape: {sample_array.shape}")
+
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# GRADED FUNCTION: image_generator
+def image_generator():
+    # Instantiate the ImageDataGenerator class and set the rescale argument.
+    train_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+
+    # Specify the method to load images from a directory and pass in the appropriate arguments:
+    # - directory: should be a relative path to the directory containing the data
+    # - target_size: set this equal to the resolution of each image (excluding the color dimension)
+    # - batch_size: number of images the generator yields when asked for a next batch. Set this to 10.
+    # - class_mode: How the labels are represented. Should be one of "binary", "categorical" or "sparse".
+    #               Pick the one that better suits here given that the labels are going to be 1D binary labels.
+    train_generator = train_datagen.flow_from_directory(directory="./data/",
+                                                        target_size=(128, 128),  # Set the target image resolution
+                                                        batch_size=10,  # Set the batch size to 10
+                                                        class_mode="binary")  # Since you have 1D binary labels
+
+    return train_generator
+
+# You can call the function to get the image generator
+train_generator = image_generator()
+# Save your generator in a variable
+gen = image_generator()
+from tensorflow.keras import optimizers, losses
+
+# GRADED FUNCTION: train_happy_sad_model
+def train_happy_sad_model(train_generator):
+    # Instantiate the callback
+    callbacks = myCallback()
+
+    ### START CODE HERE
+
+    # Define the model
+    model = tf.keras.models.Sequential([
+        # Convolutional Layer 1
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Convolutional Layer 2
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Convolutional Layer 3
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # Flatten the feature maps
+        tf.keras.layers.Flatten(),
+        # Fully Connected Layer
+        tf.keras.layers.Dense(512, activation='relu'),
+        # Output Layer
+        tf.keras.layers.Dense(1, activation='sigmoid')  # Binary classification
+
+    ])
+
+    # Compile the model
+    # Select a loss function compatible with the last layer of your network
+    model.compile(loss='binary_crossentropy',  # Since it's binary classification
+                  optimizer=optimizers.Adam(learning_rate=0.001),  # You can adjust the learning rate
+                  metrics=['accuracy'])
+
+    # Train the model
+    # Your model should achieve the desired accuracy in less than 15 epochs.
+    # You can hardcode up to 20 epochs in the function below but the callback should trigger before 15.
+    history = model.fit(train_generator,
+                        epochs=15,  # Train for up to 15 epochs
+                        callbacks=[callbacks])
+
+    ### END CODE HERE
+    return history
+hist = train_happy_sad_model(gen)
+print(f"Your model reached the desired accuracy after {len(hist.epoch)} epochs")
+if not "accuracy" in hist.model.metrics_names:
+    print("Use 'accuracy' as metric when compiling your model.")
+else:
+    print("The metric was correctly defined.")
